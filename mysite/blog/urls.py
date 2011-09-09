70e0f5e490e9ec759edfab1d3f05a84679b3ac94
Xabier Larrakoetxea Gallego <slok69@gmail.com>
2011-09-09 10:45:00 +0200

    Added Django Admin and Admindoc to the project

---
+++ mysite/blog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import patterns, include, url
+
+from blog import views
+
+urlpatterns = patterns('',
+    
+    #url(r'^$', views.blog_index),
+
+) 
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,21 @@
 from django.conf.urls.defaults import patterns, include, url
 from mysite import views, blog
 
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
+    
     url(r'^$', views.index),
     url(r'^about/$', views.about),
     url(r'^projects/$', views.projects),
     
+    # Website blog
     url(r'^blog/', include('blog.urls')),
+    
+    # Django administration
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+
 
 )
