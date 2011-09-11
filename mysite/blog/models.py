2e5fbce3e95f0ec0bb6a123ee3ba49b6b7bdfc2b
Xabier Larrakoetxea Gallego <slok69@gmail.com>
2011-09-11 12:01:06 +0200

    Changed the model for author with user foreign key

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -7,27 +7,31 @@ import datetime
 from mysite.blog.managers import PublishedManager
 
 
-def _admin_users_in_choices():
-    """
-        gets all the admin users from auth_user
-    """
-    #We need to catch and pass this error because auth_user table isn't created before this and fails when it checks,
-    #the exception thrwon is: "django.db.utils.DatabaseError: no such table: auth_user"
-    try:
-        raw_users = User.objects.all()
-        users = []
-        for i in raw_users:
-            full_name = i.username
-            users.append((full_name, full_name))
-            
-    except django.db.utils.DatabaseError:
-        pass
-        
-    return users
+#def _admin_users_in_choices():
+#    """
+#        gets all the admin users from auth_user
+#    """
+#    #We need to catch and pass this error because auth_user table isn't created before this and fails when it checks,
+#    #the exception thrwon is: "django.db.utils.DatabaseError: no such table: auth_user"
+#    try:
+#        raw_users = User.objects.all()
+#        users = []
+#        for i in raw_users:
+#            full_name = i.username
+#            users.append((full_name, full_name))
+#            
+#    except django.db.utils.DatabaseError:
+#        pass
+#        
+#    return users
+
+def _get_default_user():
+    return User.objects.get(pk=1)
+
 
 class Entry(models.Model):
     #Choices
-    user_choices = _admin_users_in_choices()
+    #user_choices = _admin_users_in_choices()
     
     status_choices = (
         ('p', 'Published'),
@@ -44,7 +48,8 @@ class Entry(models.Model):
     title = models.CharField('title', max_length=200)
     body = models.TextField()
     slug = models.SlugField('slug', unique_for_date='pub_date')
-    author = models.CharField(max_length=30, choices=user_choices)
+    #author = models.CharField(max_length=30, choices=user_choices)
+    author = models.ForeignKey(User, default=_get_default_user)
     #pub_date = models.DateTimeField('Date published', auto_now_add=True)
     pub_date = models.DateTimeField('Date published', default=datetime.datetime.now) #if we want to publish in the future we need to put the date "manually"
     mod_date =  models.DateTimeField('Date modified', auto_now=True)
