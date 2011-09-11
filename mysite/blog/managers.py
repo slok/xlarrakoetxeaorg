db4b32ca94e08a37e6a9d379b10d410e67e4f113
Xabier Larrakoetxea Gallego <slok69@gmail.com>
2011-09-11 16:05:41 +0200

    Now the entries are returned in publication date (descendig) order

--- mysite/blog/managers.py
+++ mysite/blog/managers.py
@@ -5,5 +5,7 @@ class PublishedManager(Manager):
         """Queryset to retrieve published post"""
         
         def get_query_set(self):
-            return super(PublishedManager, self).get_query_set().filter(status='p', pub_date__lte=datetime.datetime.now()) #published and in the past (future post no)
+            """get published entries and that are in the past (published future ones, aren't published in thory)
+            Orders objects by publiched date (descending) and if are conflicts with title (not normal)"""
+            return super(PublishedManager, self).get_query_set().filter(status='p', pub_date__lte=datetime.datetime.now()).order_by('-pub_date', 'title')
         
