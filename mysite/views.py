f1b0235204eec3e386d15ebec7588a41e79394f3
Xabier Larrakoetxea Gallego <slok69@gmail.com>
2011-09-15 14:59:58 +0200

    Added the latest 2 entries to the index of the site and modify the style of the page (the boxes have been removed removed)

--- mysite/views.py
+++ mysite/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render_to_response
 from django.conf import settings
 from django.template import RequestContext
 
+from mysite.blog.models import Entry
+
 def index(request):
     
+    entries = Entry.published_objects.all()[:2]
+    
     data = { 
-        '' : '',
+        'entries' : entries,
     }
     
     return render_to_response('index.html', data, context_instance=RequestContext(request))
