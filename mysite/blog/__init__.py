e023bbe6e7b5b312622e6501a410cb473148469e
Xabier Larrakoetxea <slok69@gmail.com>
2012-07-31 19:36:31 +0200

    Add basic structure of the project (closes #2)

---
+++ mysite/blog/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import settings
+
+app = Flask(__name__)
+
+#Add Config to flask 
+app.config.from_object(settings)
+
+#Import all the views
+import blog.views
---
+++ mysite/blog/views.py
@@ -0,0 +1,5 @@
+from blog import app
+
+@app.route('/')
+def index():
+    return 'Hello World!'
